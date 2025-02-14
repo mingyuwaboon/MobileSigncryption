a1a6a9c991ddd6cd5363a79d711b94cef641ef5f
mingyuwaboon <144452813+mingyuwaboon@users.noreply.github.com>
2025-02-14 22:08:00 +0700

    Update MainActivity.kt
    Modify Cloud function to comment -> waiting to send the Coud function url and delete some comment code

--- app/src/main/java/com/example/fileselecterapp/MainActivity.kt
+++ app/src/main/java/com/example/fileselecterapp/MainActivity.kt
@@ -80,8 +80,7 @@ class MainActivity : AppCompatActivity(), CloudCallback  {
                             //val data = "123456789001122"
                             val data = "0" + Head + CiKeyDATA.contentToString()
                             //val dataJSON = JSONObject(data)
-                            val url =
-                                "https://asia-southeast1-neat-veld-421803.cloudfunctions.net/function-1"
+                            val url ="" //Cloud Function Service
                             cloudservice.sendDataAndWaitForResponse(data, url, this@MainActivity)
 
                             //Log.d("CheckParallel","THe number is $COUNT")
@@ -112,8 +111,7 @@ class MainActivity : AppCompatActivity(), CloudCallback  {
                         //val data = "123456789001122"
                         val data = "0" + Head + CiKeyDATA.contentToString()
                         //val dataJSON = JSONObject(data)
-                        val url =
-                            "https://asia-southeast1-neat-veld-421803.cloudfunctions.net/function-1"
+                        val url = "" //Cloud Function Service
                         cloudservice.sendDataAndWaitForResponse(data, url, this@MainActivity)
 
                         //Log.d("CheckParallel","THe number is $COUNT")
@@ -178,8 +176,7 @@ class MainActivity : AppCompatActivity(), CloudCallback  {
                         //val data = "123456789001122"
                         val data = "0"+Head + CiKeyDATA.contentToString()
                         //val dataJSON = JSONObject(data)
-                        val url =
-                            "https://asia-southeast1-neat-veld-421803.cloudfunctions.net/function-1"
+                        val url = "" //Cloud Function Service
                         cloudservice.sendDataAndWaitForResponse(data, url, this@MainActivity)
                     }
 
@@ -229,7 +226,7 @@ class MainActivity : AppCompatActivity(), CloudCallback  {
 
             lifecycleScope.launch {
                 val data = "1$HeadSign${Sign.contentToString()}"
-                val url = "https://asia-southeast1-neat-veld-421803.cloudfunctions.net/function-1"
+                val url = ""// Cloud Function Service
                 cloudservice.sendDataAndWaitForResponse(data, url, this@MainActivity)
             }
             dataSent = true
@@ -259,181 +256,3 @@ fun stringToHex(input: String): String {
 fun concatenateByteArrays(array1: ByteArray, array2: ByteArray): ByteArray {
     return array1 + array2
 }
-
-
-
-
-/*
-class MainActivity : AppCompatActivity(), CloudCallback  {
-
-    val startTime = System.nanoTime()
-    private val FILE_PICKER_REQUEST_CODE = 1
-    private lateinit var fileview: TextView
-    private lateinit var MGview: TextView
-    private lateinit var Decryptfile: TextView
-    private lateinit var Signview: TextView
-    private lateinit var contentview: TextView
-    private lateinit var DATASEND: ByteArray
-    private var responseData: String =""
-    private var dataSent = false
-
-
-
-    private val KeyBoss =  CryptoManager()
-    val cloudservice = Cloud()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        fileview = findViewById(R.id.fileviewuri)
-        MGview = findViewById(R.id.MGview)
-        Decryptfile = findViewById(R.id.Decryptview)
-        Signview = findViewById(R.id.SignView)
-        contentview = findViewById(R.id.Contentview)
-
-        val chooseFileButton = findViewById<Button>(R.id.selectbutton)
-        chooseFileButton.setOnClickListener {
-            openFilePicker()
-        }
-
-    }
-
-
-    private fun openFilePicker() {
-        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-        intent.addCategory(Intent.CATEGORY_OPENABLE)
-        intent.type = "application/pdf"
-        startActivityForResult(intent, FILE_PICKER_REQUEST_CODE)
-    }
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == FILE_PICKER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-
-            // Handle selected file URI
-            val selectedFileUri = data?.data
-
-            // Evaluate test
-            val time = Evaluate().measureTimeMillis {
-
-                // Use the selected file URI as needed
-                selectedFileUri?.let {
-
-                    //Read content to ByteArray
-                    val fileContent = FileHelper.uriToByteArray(this, it)
-
-                    //OTP
-                    val key = KeyBoss.generateRandomKey(fileContent!!.size)
-
-                    val encryptedContent = KeyBoss.oneTimePadEncrypt(fileContent, key)
-                    val decryptedContent = KeyBoss.oneTimePadDecrypt(encryptedContent!!, key)
-
-                    val CiKeyDATA = Header().GenerateDATA(encryptedContent, key)
-                    DATASEND = CiKeyDATA
-
-                    /*
-                    //Hash - MD
-                    val MDigest = KeyBoss.hashByteArrayWithSHA256(CiKeyDATA)
-
-
-                    //ECC -Signature
-                    val alias_ecc = "my_ecc_key"
-
-                    val keyecc = KeyBoss.generateECCKeyPair(this,alias_ecc)
-                    val Sign = KeyBoss.signData(keyecc.private, MDigest)
-
-                    //RSA
-
-                    val alias_rsa = "my_rsa_key"
-                    KeyBoss.generateAndStoreRSAKeyPair(this,alias_rsa)
-
-                    val SendDATA = concatenateByteArrays(Sign, CiKeyDATA)
-
-                    val FinalData = KeyBoss.encryptLargeDataWithRSA(alias_rsa, SendDATA)
-                    */
-
-
-                    val Head = Header().GenHeader(key.size)
-                    //First data sending
-
-                    lifecycleScope.launch {
-                        //val data = "{\"Head\": \"$Head\", \"Data\": \"$CiKeyDATA\"}"
-                        //val data = "{\"Head\": \"THIS IS HEADER\", \"Data\": \"THIS IS KEY\"}"
-                        //val data = "123456789001122"
-                        val data = "0"+Head + CiKeyDATA.contentToString()
-                        //val dataJSON = JSONObject(data)
-                        val url =
-                            "https://asia-southeast1-neat-veld-421803.cloudfunctions.net/function-1"
-                        cloudservice.sendDataAndWaitForResponse(data, url, this@MainActivity)
-                    }
-
-
-                    //Log.d("Check", "------Finish All process------")
-
-                    //Sending Data to cloud
-                    //val Send2Cloud = Header().encapsulation(encryptedContent,key)
-                    //Toast.makeText(this@MainActivity,"$Send2Cloud",Toast.LENGTH_SHORT).show()
-
-
-//                    Decryptfile.text = responseData //Decrypt
-//                    fileview.text = it.toString() // Convert Uri to String
-//                    MGview.text = MDigest.contentToString()  // MG
-//                    Signview.text = Sign.contentToString() //Sign
-//                    contentview.text = FinalData.contentToString() // Content in file
-                    //Contextfile.text = FileHelper.readFileContent(this, it) // read to represent content
-                }
-
-            }
-            val test = "123456789"
-            val gg = test.substring(4)
-            val ge = test.substring(0,5)
-            Log.d("Check","This is test0,5: $ge")
-            Log.d("Check","This is test4: $gg")
-            Log.d("Check","------The usage time is: $time ------")
-        }
-    }
-
-    override fun onResponse(response: String) {
-        // Use the response here
-        responseData = response
-
-        Log.d("Response5556", response)
-        //val jsonObject = JSONObject(response)
-        //responseData = jsonObject.getString("key")
-        if (!dataSent && responseData != "") {
-            // Send data to the cloud again
-            val HeadSign = responseData.substring(0, 12)
-            val MG = responseData.substring(12).toByteArray()
-
-            val alias_ecc = "my_ecc_key"
-            val keyecc = KeyBoss.generateECCKeyPair(this, alias_ecc)
-            val Sign = KeyBoss.signData(keyecc.private, MG)
-
-
-            lifecycleScope.launch {
-                val data = "1$HeadSign${Sign.contentToString()}"
-                val url = "https://asia-southeast1-neat-veld-421803.cloudfunctions.net/function-1"
-                cloudservice.sendDataAndWaitForResponse(data, url, this@MainActivity)
-            }
-            dataSent = true
-            MGview.text = MG.contentToString()  // MG
-            Signview.text = Sign.contentToString() //Sign
-            Log.d("Check", "------Finish All process------")
-        }
-        else {
-            // dataSent = true then set response
-            Decryptfile.text = responseData //Decrypt
-            //fileview.text = it.toString() // Convert Uri to String
-            //contentview.text = responseData // Content in file
-            val EndTime = System.nanoTime()
-            val elapsedTimeNano = EndTime - startTime
-            Log.d("check","\"------The usage time is: ${elapsedTimeNano / 1_000_000} ------")
-        }
-    }
-
-
-
-}
-*/
